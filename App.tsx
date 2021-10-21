ad72443fb1d61a96c1d60a5bb82458dff905a09e
Carlito <rody.castillo.tacuri@gmail.com>
2021-10-20 21:54:31 -0500

    Config environments

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+
+import React from 'react'
+import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      
+    </NavigationContainer>
+  )
+}
+
+export default App
